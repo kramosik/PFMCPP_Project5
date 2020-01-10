25386c3098a55e37aaf6439912ee8dfc61a1b2a1
kramosik <59625838+kramosik@users.noreply.github.com>
2020-01-10 21:19:02 +0000

    Fix styles

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 /*
  Chapter 3 Part 1 - Continuation of previous Task
  The 'this' keyword.
- The purpose of this task is to demonstrate the difference between using member variables in member
-functions, vs. using member variables in free functions.
+ The purpose of this task is to demonstrate the difference between using member
+variables in member functions, vs. using member variables in free functions.
 
 1) write 5 UDTs.
     a) declare and implement:
@@ -10,26 +10,29 @@ functions, vs. using member variables in free functions.
         destructors,
         2+ member functions with an arbitrary number of parameters, and
         3+ member variables, initialized in-class
-    b) the previous tasks required you to write std::cout statements in main() that accessed the
-member variables, or printed out the results from calling member functions. write 2 std::cout
-statements in main() that do this for each UDT.
+    b) the previous tasks required you to write std::cout statements in main()
+that accessed the member variables, or printed out the results from calling
+member functions. write 2 std::cout statements in main() that do this for each
+UDT.
 
- 2) For any std::cout statements you had in main() that accessed member variables of your types or
-printed out the results of your member function calls, a) write a member function that prints the
-same thing out, but uses the proper techniques inside the member functions to access the same member
-variables/functions. b) be explicit with your use of 'this->' in those member functions so we see
-how you're accessing/calling those member variables and functions *inside* c) call those member
-functions after your std::cout statements in main().  see the example below as to what I'm expecting
-you to write.
+ 2) For any std::cout statements you had in main() that accessed member
+variables of your types or printed out the results of your member function
+calls, a) write a member function that prints the same thing out, but uses the
+proper techniques inside the member functions to access the same member
+variables/functions. b) be explicit with your use of 'this->' in those member
+functions so we see how you're accessing/calling those member variables and
+functions *inside* c) call those member functions after your std::cout
+statements in main().  see the example below as to what I'm expecting you to
+write.
 
- 3) if you didn't have any std::cout statements in main() that accessed member variables:
-         write some.
-         then do 2a) 2b) 2c)
+ 3) if you didn't have any std::cout statements in main() that accessed member
+variables: write some. then do 2a) 2b) 2c)
 
- 4) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
+ 4) After you finish, click the [run] button.  Clear up any errors or warnings
+as best you can.
 
- Commit your changes by clicking on the Source Control panel on the left, entering a message, and
-click [Commit and push].
+ Commit your changes by clicking on the Source Control panel on the left,
+entering a message, and click [Commit and push].
 
  Wait for my code review.
  */
@@ -38,24 +41,37 @@ click [Commit and push].
  example:
  */
 #include <iostream>
-namespace example {
-struct MyFoo {
-    MyFoo() { std::cout << "creating MyFoo" << std::endl; }
-    ~MyFoo() { std::cout << "destroying MyFoo" << std::endl; }
+namespace example
+{
+struct MyFoo
+{
+    MyFoo()
+    {
+        std::cout << "creating MyFoo" << std::endl;
+    }
+    ~MyFoo()
+    {
+        std::cout << "destroying MyFoo" << std::endl;
+    }
     void memberFunc()
     {
         std::cout << "MyFoo returnValue(): " << this->returnValue()
-                  << " and MyFoo memberVariable: " << this->memberVariable << std::endl;
+                  << " and MyFoo memberVariable: " << this->memberVariable
+                  << std::endl;
     } // 2a)
-    int returnValue() { return 3; }
+    int returnValue()
+    {
+        return 3;
+    }
     float memberVariable = 3.14f;
 };
 int main()
 {
     MyFoo mf;
     std::cout << "mf returnValue(): " << mf.returnValue()
-              << " and mf memberVariable: " << mf.memberVariable << std::endl; // 1b)
-    mf.memberFunc();                                                           // 2c)
+              << " and mf memberVariable: " << mf.memberVariable
+              << std::endl; // 1b)
+    mf.memberFunc();        // 2c)
     return 0;
 }
 } // namespace example
@@ -64,16 +80,27 @@ int main()
  UDT 1:
  */
 #include <string>
-struct Page {
-    Page() : chapter(""), content("") {}
+struct Page
+{
+    Page() : chapter(""), content("")
+    {
+    }
     Page(int pageNumber, std::string chapter, std::string content)
         : pageNumber(pageNumber), chapter(chapter), content(content)
     {
     }
-    ~Page() {}
+    ~Page()
+    {
+    }
 
-    std::string getText() { return chapter + '\n' + content + '\n' + getPageNumber() + '\n'; }
-    std::string getPageNumber() { return std::to_string(pageNumber); }
+    std::string getText()
+    {
+        return chapter + '\n' + content + '\n' + getPageNumber() + '\n';
+    }
+    std::string getPageNumber()
+    {
+        return std::to_string(pageNumber);
+    }
 
     void memberFunc()
     {
@@ -89,22 +116,30 @@ struct Page {
 /*
  UDT 2:
  */
-struct Lamp {
-    Lamp() : color("white") {}
+struct Lamp
+{
+    Lamp() : color("white")
+    {
+    }
     Lamp(float brightness, float battery, std::string color)
         : brightness(brightness), battery(battery), color(color)
     {
     }
-    ~Lamp() {}
+    ~Lamp()
+    {
+    }
 
-    std::string getLight() { return color + ": " + std::to_string(brightness); }
+    std::string getLight()
+    {
+        return color + ": " + std::to_string(brightness);
+    }
     void toggle()
     {
-        if (brightness != 0) 
+        if (brightness != 0)
         {
             brightness = 100;
-        } 
-        else 
+        }
+        else
         {
             brightness = 0;
         }
@@ -112,7 +147,8 @@ struct Lamp {
 
     void memberFunc()
     {
-        std::cout << "Lamp getLight(): " << this->getLight() << "\nLamp battery: " << this->battery << '\n';
+        std::cout << "Lamp getLight(): " << this->getLight()
+                  << "\nLamp battery: " << this->battery << '\n';
     }
 
     float brightness = 0;
@@ -124,13 +160,18 @@ struct Lamp {
  UDT 3:
  */
 #include <sstream>
-struct Metronome {
-    Metronome() {}
+struct Metronome
+{
+    Metronome()
+    {
+    }
     Metronome(float tempo, int beatsPerMeasure, bool started)
         : tempo(tempo), beatsPerMeasure(beatsPerMeasure), started(started)
     {
     }
-    ~Metronome() {}
+    ~Metronome()
+    {
+    }
 
     std::string getStatus()
     {
@@ -148,12 +189,17 @@ struct Metronome {
                     os << '.';
                 }
             }
-        } else {
+        }
+        else
+        {
             os << "metronome not started\n";
         }
         return os.str();
     }
-    void toggle() { started = !started; }
+    void toggle()
+    {
+        started = !started;
+    }
 
     void memberFunc()
     {
@@ -169,16 +215,27 @@ struct Metronome {
 /*
  UDT 4:
  */
-struct Screen {
-    Screen() : manufacturer("") {}
+struct Screen
+{
+    Screen() : manufacturer("")
+    {
+    }
     Screen(int height, int width, std::string manufacturer)
         : height(height), width(width), manufacturer(manufacturer)
     {
     }
-    ~Screen() {}
+    ~Screen()
+    {
+    }
 
-    int getNumofPixels() { return height * width; }
-    bool coordInBound(int x, int y) { return (x > 0 && x < width) && (y > 0 && y < height); }
+    int getNumofPixels()
+    {
+        return height * width;
+    }
+    bool coordInBound(int x, int y)
+    {
+        return (x > 0 && x < width) && (y > 0 && y < height);
+    }
 
     void memberFunc()
     {
@@ -194,16 +251,27 @@ struct Screen {
 /*
  UDT 5:
  */
-struct File {
-    File() : filename(""), directory("") {}
+struct File
+{
+    File() : filename(""), directory("")
+    {
+    }
     File(std::string filename, std::string directory, long sizeInBytes)
         : filename(filename), directory(directory), sizeInBytes(sizeInBytes)
     {
     }
-    ~File() {}
+    ~File()
+    {
+    }
 
-    std::string getPath() { return directory + "/" + filename; }
-    double sizeInKB() { return sizeInBytes / 1024.0; }
+    std::string getPath()
+    {
+        return directory + "/" + filename;
+    }
+    double sizeInKB()
+    {
+        return sizeInBytes / 1024.0;
+    }
 
     void memberFunc()
     {
@@ -223,12 +291,14 @@ int main()
 
     // UDT 1:
     Page page {1, "Chapter 1", "Some random text"};
-    std::cout << "page getText(): " << page.getText() << "page pageNumber: " << page.pageNumber << '\n';
+    std::cout << "page getText(): " << page.getText()
+              << "page pageNumber: " << page.pageNumber << '\n';
     page.memberFunc();
 
     // UDT 2:
     Lamp lamp {20, 50, "red"};
-    std::cout << "lamp getLight(): " << lamp.getLight() << "\nlamp battery: " << lamp.battery << '\n';
+    std::cout << "lamp getLight(): " << lamp.getLight()
+              << "\nlamp battery: " << lamp.battery << '\n';
     lamp.memberFunc();
 
     // UDT 3:
@@ -245,7 +315,8 @@ int main()
 
     // UDT 5:
     File file {"main.cpp", "/home", 10000};
-    std::cout << "file getPath(): " << file.getPath() << "\nfile sizeInBytes: " << file.sizeInBytes << '\n';
+    std::cout << "file getPath(): " << file.getPath()
+              << "\nfile sizeInBytes: " << file.sizeInBytes << '\n';
     file.memberFunc();
 
     return 0;
